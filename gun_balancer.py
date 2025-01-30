ab6a3fa5f5506d15a4debaf09ac1149dba5e279a
MrXanter <orimakc2@gmail.com>
2025-01-30 21:48:41 +0100

    added diapasons

--- gun_balancer.py
+++ gun_balancer.py
@@ -1,5 +1,17 @@
 def variate_gun():
-    dmg = print(f'Maximal damage of a gun(write full number for example: 43.2): {input(float)}')
-    fr = print(f'Gun fire rate(bullets per minute): {input(float)}')
-    diap_dmg = print(f'Choose the diapason in which the damage will be varied(a typed number means +-number from damage){input(float)}')
-    diap_fr = print(f'Choose the diapason in which the fire rate will be varied(a typed number means +-number from fire rate){input(float)}')
+    dmg = print(f'Maximal damage of a gun(write full number for example: 43.2): {abs(input(float))}')
+    
+    fr = print(f'Gun fire rate(bullets per minute): {abs(input(float))}')
+    
+    dps = ( float(dmg) * float(fr) ) / 60
+    
+    diap_dmg = print(f'Choose the diapason in which the damage will be varied(a typed number means +-number from damage){abs(input(float))}')
+    
+    dmg_min = dmg - diap_dmg 
+    dmg_max = dmg + diap_dmg
+    if diap_dmg > dmg:
+        print("The diapason of damage is out of range(Damage - diapason of damage < 0)")
+    
+    diap_fr = print(f'Choose the diapason in which the fire rate will be varied(a typed number means +-number from fire rate){abs(input(float))}')
+    if diap_fr > fr:
+        print("The diapason of fire rate is out of range(Fire rate - diapason of fire rate < 0) ")
