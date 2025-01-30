bb2418e1df8537b5378ec4034ce2064a55387a02
MrXanter <orimakc2@gmail.com>
2025-01-30 21:17:38 +0100

    commited to see saves

---
+++ gun_balancer.py
@@ -0,0 +1,5 @@
+def variate_gun():
+    dmg = print(f'Maximal damage of a gun(write full number for example: 43.2): {input(float)}')
+    fr = print(f'Gun fire rate(bullets per minute): {input(float)}')
+    diap_dmg = print(f'Choose the diapason in which the damage will be varied(a typed number means +-number from damage){input(float)}')
+    diap_fr = print(f'Choose the diapason in which the fire rate will be varied(a typed number means +-number from fire rate){input(float)}')
