2f673107199747bca540aa39cc4474fd2e198cfc
MrXanter <orimakc2@gmail.com>
2025-01-30 22:06:36 +0100

    end of the day commit

--- gun_balancer.py
+++ gun_balancer.py
@@ -1,17 +1,18 @@
-def variate_gun():
+def variate_gun_stats():
     dmg = print(f'Maximal damage of a gun(write full number for example: 43.2): {abs(input(float))}')
     
     fr = print(f'Gun fire rate(bullets per minute): {abs(input(float))}')
     
-    dps = ( float(dmg) * float(fr) ) / 60
+    dps = ( float(dmg) * float(fr) ) / 60 # Damage per second
     
     diap_dmg = print(f'Choose the diapason in which the damage will be varied(a typed number means +-number from damage){abs(input(float))}')
-    
     dmg_min = dmg - diap_dmg 
     dmg_max = dmg + diap_dmg
-    if diap_dmg > dmg:
+    if dmg_min < 0:
         print("The diapason of damage is out of range(Damage - diapason of damage < 0)")
     
     diap_fr = print(f'Choose the diapason in which the fire rate will be varied(a typed number means +-number from fire rate){abs(input(float))}')
-    if diap_fr > fr:
+    fr_min = fr - diap_fr 
+    fr_max = fr + diap_fr
+    if fr_min < 0:
         print("The diapason of fire rate is out of range(Fire rate - diapason of fire rate < 0) ")
