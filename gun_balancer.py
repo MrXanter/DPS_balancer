faf4a6285560e9e88883bbae54fa59f3395167a8
MrXanter <orimakc2@gmail.com>
2025-01-31 13:09:27 +0100

    added vscode folder

--- gun_balancer.py
+++ gun_balancer.py
@@ -1,18 +1,26 @@
 def variate_gun_stats():
-    dmg = print(f'Maximal damage of a gun(write full number for example: 43.2): {abs(input(float))}')
-    
-    fr = print(f'Gun fire rate(bullets per minute): {abs(input(float))}')
-    
-    dps = ( float(dmg) * float(fr) ) / 60 # Damage per second
-    
-    diap_dmg = print(f'Choose the diapason in which the damage will be varied(a typed number means +-number from damage){abs(input(float))}')
-    dmg_min = dmg - diap_dmg 
+    dmg = abs(float(input("Maximal damage of a gun (write full number, e.g., 43.2): ")))
+    fr = abs(float(input("Gun fire rate (bullets per minute): ")))
+
+    dps = (dmg * fr) / 60  # Damage per second
+
+    diap_dmg = abs(float(input("Choose the damage variation range (+- value): ")))
+    dmg_min = dmg - diap_dmg
     dmg_max = dmg + diap_dmg
+
     if dmg_min < 0:
-        print("The diapason of damage is out of range(Damage - diapason of damage < 0)")
+        print("The damage range is out of bounds (damage - variation < 0).")
     
-    diap_fr = print(f'Choose the diapason in which the fire rate will be varied(a typed number means +-number from fire rate){abs(input(float))}')
-    fr_min = fr - diap_fr 
+    diap_fr = abs(float(input("Choose the fire rate variation range (+- value): ")))
+    fr_min = fr - diap_fr
     fr_max = fr + diap_fr
+
     if fr_min < 0:
-        print("The diapason of fire rate is out of range(Fire rate - diapason of fire rate < 0) ")
+        print("The fire rate range is out of bounds (fire rate - variation < 0).")
+
+    list_damage = []  # Initialize the list
+    for i in range(int(dmg_min), int(dmg_max)):  # Convert to int for range
+        list_damage.append(i)
+    
+    print("Damage variations:", list_damage)
+
